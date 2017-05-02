4b57d229fa6e6a1890fefd57cc9d496a88cd2cf5
stephenliu <stephenlyao@yeah.net>
2017-05-02 23:42:24 +0800

    init folder

---
+++ slushfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp')
+const install = require('gulp-install')
+const conflict = require('gulp-conflict')
+const rename = require('gulp-rename')
+const template = require('gulp-template')
+const inquirer = require('inquirer')
+
+
